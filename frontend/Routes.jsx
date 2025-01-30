75dd952a5a5033dc8263618979756377b491ce15
Angel <ANGEL.MARY@kalvium.community>
2025-01-30 09:00:14 +0530

    milestone 9

--- frontend/Routes.jsx
+++ frontend/Routes.jsx
@@ -1,4 +1,5 @@
 import LoginPage from '../frontend/src/pages/Login'
 import SignupPage from '../frontend/src/pages/Signup'
 import Home from "../frontend/src/pages/Home";
-export {LoginPage,SignupPage,Home};
+import CreateProduct from '../frontend/src/pages/createProduct';
+export {LoginPage,SignupPage,Home,CreateProduct};

ba608cc7fdd5782eb101ffea0bf3187f88b8eded
Angel <ANGEL.MARY@kalvium.community>
2025-02-04 16:48:04 +0530

    milestone 12 front

--- frontend/Routes.jsx
+++ frontend/Routes.jsx
@@ -2,4 +2,5 @@ import LoginPage from '../frontend/src/pages/Login'
 import SignupPage from '../frontend/src/pages/Signup'
 import Home from "../frontend/src/pages/Home";
 import CreateProduct from '../frontend/src/pages/createProduct';
-export {LoginPage,SignupPage,Home,CreateProduct};
+import MyProducts from "./pages/myProducts.jsx";
+export {LoginPage,SignupPage,Home,CreateProduct,MyProducts};

5da7b97a2d4e0a221e7e500c6f93dfb985b56ed9
Angel <ANGEL.MARY@kalvium.community>
2025-02-08 12:35:55 +0530

    m15

--- frontend/Routes.jsx
+++
@@ -1,6 +0,0 @@
-import LoginPage from '../frontend/src/pages/Login';
-import SignupPage from '../frontend/src/pages/Signup';
-import Home from "../frontend/src/pages/Home";
-import CreateProduct from '../frontend/src/pages/createProduct';
-import MyProducts from "../frontend/src/pages/myProdcucts";
-export {LoginPage,SignupPage,Home,CreateProduct,MyProducts};
